f5213803449527a82789eda752073c10058a0bc9
sanilpatankar <sanilpatankar@gmail.com>
2016-08-20 19:15:50 -0400

    Update portfolio.js

--- client/js/portfolio.js
+++ client/js/portfolio.js
@@ -2,7 +2,7 @@
  * Created by sanil on 8/18/2016.
  */
 
-angular.module("myModule",[])
+var portfolio = angular.module("myModule",[])
     .controller("myController", function ($scope, $http) {
         $scope.formData = {};
         $scope.formSubmit = function () {

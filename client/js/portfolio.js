b83c5e8576711d0ac0ad8da62316ace836d21c7e
sanil <sanilpatankar@gmail.com>
2016-08-20 22:40:42 -0400

    8/20/2016

--- client/js/portfolio.js
+++ client/js/portfolio.js
@@ -1,13 +1,13 @@
 /**
  * Created by sanil on 8/18/2016.
  */
-/*
-angular.module("myModule",[])
+
+/*angular.module("myModule",[])
     .controller("myController", function ($scope, $http) {
         $scope.formData = {};
         $scope.formSubmit = function () {
             console.log('inside'+JSON.stringify($scope.formData));
         };
-    });
+    });*/
 
-*/
+
